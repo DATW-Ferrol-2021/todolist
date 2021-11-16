1d6bf4b91d6e292b6687e40dfa1f5b8fc45321b8
Jairo Chapela-Martinez <jairochapela@gmail.com>
2021-11-16 19:23:31 +0100

    Engadir tarefa e actualizar lista

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const elemTarefas = document.getElementById("tarefas");
+const formulario = document.getElementById("formulario");
+
 var tarefas = [
     "Unha de proba"
 ];
 
 
 function mostrarTarefas() {
+    elemTarefas.innerHTML = "";
     tarefas.forEach(t => {
         let nodo = document.createElement("li");
         nodo.innerText = t;
@@ -12,5 +15,11 @@ function mostrarTarefas() {
     })
 }
 
+function engadirTarefa(event) {
+    tarefas.push(formulario.tarefa.value);
+    mostrarTarefas();
+    formulario.reset();
+    return false;
+}
 
 mostrarTarefas();
