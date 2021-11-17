fd15507d748d8c406b27c5d608e6b550b9eee1a9
Jairo Chapela-Martinez <jairochapela@gmail.com>
2021-11-17 09:08:30 +0100

    Checkboxes y labels.

--- app.js
+++ app.js
@@ -8,10 +8,19 @@ var tarefas = [
 
 function mostrarTarefas() {
     elemTarefas.innerHTML = "";
-    tarefas.forEach(t => {
-        let nodo = document.createElement("li");
-        nodo.innerText = t;
-        elemTarefas.appendChild(nodo);
+    tarefas.forEach((t,i) => {
+        let checkId = "chk"+i;
+        let item = document.createElement("li");
+        let checkbox = document.createElement("input");
+        checkbox.setAttribute("type", "checkbox");
+        //checkbox.setAttribute("name", checkId);
+        checkbox.setAttribute("id", checkId);
+        let label = document.createElement("label");
+        label.innerText = t;
+        label.setAttribute("for", checkId);
+        item.appendChild(checkbox);
+        item.appendChild(label);
+        elemTarefas.appendChild(item);
     })
 }
 
